80981f0fb7305092afb927fc9669230f9edd301e
guillaume.bouvier <guillaume.bouvier@gameloft.com>
2020-06-05 11:26:28 +0900

    initial commit, part0 & part1

---
+++ engine.py
@@ -0,0 +1,53 @@
+import sys
+import os
+os.environ["path"] = os.path.dirname(sys.executable) + ";" + os.environ["path"]
+import glob
+
+import tcod as libtcod
+from input_handlers import handle_keys
+
+DATA_FOLDER = "data"
+FONT_FILE = os.path.join(DATA_FOLDER, "arial10x10.png")
+
+def main():
+	screen_width = 80
+	screen_height = 50
+	
+	player_x = int(screen_width / 2)
+	player_y = int(screen_height / 2)
+	
+	libtcod.console_set_custom_font(FONT_FILE, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+	libtcod.console_init_root(screen_width, screen_height, "ombarus libtcod tutorial revised", False)
+	con = libtcod.console_new(screen_width, screen_height)
+	
+	key = libtcod.Key()
+	mouse = libtcod.Mouse()
+	
+	while not libtcod.console_is_window_closed():
+		libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+		libtcod.console_set_default_foreground(con, libtcod.white)
+		libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+		libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+		libtcod.console_flush()
+		
+		libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+		
+		action = handle_keys(key)
+		
+		move = action.get("move")
+		exit = action.get("exit")
+		fullscreen = action.get("fullscreen")
+		
+		if move:
+			dx, dy = move
+			player_x += dx
+			player_y += dy
+			
+		if exit:
+			return True
+			
+		if fullscreen:
+			libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+	
+if __name__ == "__main__":
+	main()
